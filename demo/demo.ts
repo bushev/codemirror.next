dbba6a91bdb9bd9ca4d56dadaa06694ef7aaff25
Yury Bushev <bushevuv@gmail.com>
2021-05-10 07:31:00 -0400

    Shows the cursor issue.

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,9 +1,20 @@
 import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
-import {javascript} from "@codemirror/lang-javascript"
+import {python} from "@codemirror/lang-python"
+import {setDiagnostics} from "@codemirror/lint";
 
-let state = EditorState.create({doc: 'console.log("Hello world")', extensions: [
-  basicSetup,
-  javascript(),
-]})
+let state = EditorState.create({doc: `# This is a basic program on Python
+print('Hello, World!')`, extensions: [
+    basicSetup,
+    python()
+  ]})
 
 ;(window as any).view = new EditorView({state, parent: document.querySelector("#editor")!})
+
+setTimeout(() => {
+  (window as any).view.dispatch(setDiagnostics((window as any).view.state, [{
+    from: 58,
+    message: "W292 no newline at end of file",
+    severity: "warning",
+    to: 58
+  }]));
+}, 500);
